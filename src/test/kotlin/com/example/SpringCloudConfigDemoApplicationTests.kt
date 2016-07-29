2c682ae64e72477db18296af5236b5127ff1c43a
Michael Oleske <moleske@gmail.com>
2016-07-28 20:27:52 -0700

    bump to sr4 and 1.4.0 because why not

--- src/test/kotlin/com/example/SpringCloudConfigDemoApplicationTests.kt
+++ src/test/kotlin/com/example/SpringCloudConfigDemoApplicationTests.kt
@@ -2,11 +2,11 @@ package com.example
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 @RunWith(SpringJUnit4ClassRunner::class)
-@SpringApplicationConfiguration(classes = arrayOf(SpringCloudConfigDemoApplication::class))
+@SpringBootTest(classes = arrayOf(SpringCloudConfigDemoApplication::class))
 class SpringCloudConfigDemoApplicationTests {
 
     @Test
